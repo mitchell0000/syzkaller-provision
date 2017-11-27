fad33faa1df1de26c12225e6e8638a4041cbbe65
HEQING YAN <scottieyan@gmail.com>
2017-11-27 17:43:30 -0500

    correct port

--- host_vm_setup.sh
+++ host_vm_setup.sh
@@ -4,7 +4,7 @@
 
 unxz FreeBSD-12.0-CURRENT-amd64.qcow2.xz
 
-qemu-system-x86_64 -m 2048 -net nic -net user,host=10.0.2.10,hostfwd=tcp::37865-:22 -display none -serial stdio -no-reboot -numa node,nodeid=0,cpus=0-1 -numa node,nodeid=1,cpus=2-3 -smp sockets=2,cores=2,threads=1 -enable-kvm -hda /root/FreeBSD-12.0-CURRENT-amd64.qcow2 &
+qemu-system-x86_64 -m 2048 -net nic -net user,host=10.0.2.10,hostfwd=tcp::10022-:22 -display none -serial stdio -no-reboot -numa node,nodeid=0,cpus=0-1 -numa node,nodeid=1,cpus=2-3 -smp sockets=2,cores=2,threads=1 -enable-kvm -hda /root/FreeBSD-12.0-CURRENT-amd64.qcow2 &
 
 chmod +x ./bootstrap_img.sh
 chmod 400 ~/.ssh/kaller_key
