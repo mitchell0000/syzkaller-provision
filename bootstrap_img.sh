6dcc819e706299bb38d988dc46e25768c5c93c29
HEQING YAN <scottieyan@gmail.com>
2017-11-27 11:42:12 -0500

    configure host Linux env obtain executor from FreeBSD guest

---
+++ bootstrap_img.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+env ASSUME_ALWAYS_YES=YES pkg bootstrap
+env ASSUME_ALWAYS_YES=YES pkg install git
+
+cd ~ && git clone https://github.com/google/syzkaller
+cd ~/syzkaller && c++ executor/executor_freebsd.cc -o syz-executor -O1 -lpthread -DGOOS=\"freebsd\" -DGIT_REVISION=\"$(git rev-parse HEAD)\"
+cp ~/syzkaller/syz-executor ~/
