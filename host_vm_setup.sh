3480019244f9519eaebd6dbb73e7a30983feb244
HEQING YAN <scottieyan@gmail.com>
2017-11-27 18:14:09 -0500

    fix path add TODO

--- host_vm_setup.sh
+++ host_vm_setup.sh
@@ -2,11 +2,13 @@
 
 # configure vm and image
 
-unxz FreeBSD-12.0-CURRENT-amd64.qcow2.xz
+unxz ~/FreeBSD-12.0-CURRENT-amd64.qcow2.xz
 
-qemu-system-x86_64 -m 2048 -net nic -net user,host=10.0.2.10,hostfwd=tcp::10022-:22 -display none -serial stdio -no-reboot -numa node,nodeid=0,cpus=0-1 -numa node,nodeid=1,cpus=2-3 -smp sockets=2,cores=2,threads=1 -enable-kvm -hda /root/FreeBSD-12.0-CURRENT-amd64.qcow2 &
+qemu-system-x86_64 -m 2048 -net nic -net user,host=10.0.2.10,hostfwd=tcp::10022-:22 -display none -serial stdio -no-reboot -numa node,nodeid=0,cpus=0-1 -numa node,nodeid=1,cpus=2-3 -smp sockets=2,cores=2,threads=1 -enable-kvm -hda ~/FreeBSD-12.0-CURRENT-amd64.qcow2 &
+## need to fix qemu not running in background in script
 
 chmod +x ./bootstrap_img.sh
+cp ./kaller_key ~/.ssh/kaller_key
 chmod 400 ~/.ssh/kaller_key
 
 scp -o StrictHostKeyChecking=no -i ~/.ssh/kaller_key -P 10022 ./bootstrap_img.sh root@localhost:~/
