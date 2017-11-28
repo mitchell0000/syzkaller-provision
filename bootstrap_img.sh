ce59865cb85aae6ffe46105b160d5ed028cb2ea6
HEQING YAN <scottieyan@gmail.com>
2017-11-28 17:51:35 +0000

    syscall description does not match, try gcc as recommended in the docs

--- bootstrap_img.sh
+++ bootstrap_img.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 env ASSUME_ALWAYS_YES=YES pkg bootstrap
-env ASSUME_ALWAYS_YES=YES pkg install git
+env ASSUME_ALWAYS_YES=YES pkg install git gcc
 
 git clone https://github.com/ScottieY/syzkaller ~/syzkaller
 
-cd ~/syzkaller && c++ executor/executor_freebsd.cc -o syz-executor -O1 -lpthread -DGOOS=\"freebsd\" -DGIT_REVISION=\"$(git rev-parse HEAD)\"
+cd ~/syzkaller && gcc executor/executor_freebsd.cc -o syz-executor -O1 -lpthread -DGOOS=\"freebsd\" -DGIT_REVISION=\"$(git rev-parse HEAD)\"
 cp ~/syzkaller/syz-executor ~/
