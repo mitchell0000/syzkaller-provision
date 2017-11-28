5c8b42066316e6efb37922356381e52e4f8e79cc
HEQING YAN <scottieyan@gmail.com>
2017-11-28 16:27:51 +0000

    qemu wont work headless unless using ssh -t directly

--- create_instance.sh
+++ create_instance.sh
@@ -5,10 +5,17 @@ eval $(docker-machine env kaller)
 
 
 docker-machine scp ./host_env_setup.sh root@kaller:~/
-docker-machine ssh kaller 'chmod +x ~/provision/host_env_setup.sh && ./provision/host_vm_setup.sh'
+docker-machine ssh kaller 'chmod +x ~/host_env_setup.sh && ./host_env_setup.sh'
 
 #start provision the linux host machine
 docker-machine scp ./FreeBSD-12.0-CURRENT-amd64.qcow2.xz root@kaller:~/
 docker-machine ssh kaller 'git clone https://github.com/ScottieY/syzkaller-provision ~/provision'
 
+docker-machine ssh kaller -t 'chmod +x ~/provision/obtain_resources.sh && ~/provision/obtain_resources.sh'
+
+docker-machine ssh kaller -t 'chmod +x ~/provision/host_vm_setup.sh && ~/provision/host_vm_setup.sh'
+
 echo "Instance created."
+
+
+docker-machine scp ./freebsd.cfg root@kaller:~/
