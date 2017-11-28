4bbd2cebf6f51320ab85567156fbea7a59515c82
HEQING YAN <scottieyan@gmail.com>
2017-11-28 16:56:09 +0000

    remove unused line

--- bootstrap_img.sh
+++ bootstrap_img.sh
@@ -4,8 +4,5 @@ env ASSUME_ALWAYS_YES=YES pkg install git
 
 cd ~ && git clone https://github.com/ScottieY/syzkaller
 
-GITREV=$(git rev-parse HEAD)
-[ "$(git diff --shortstat)" == "" ] && REV=$GITREV || REV=$GITREV+
-
 cd ~/syzkaller && c++ executor/executor_freebsd.cc -o syz-executor -O1 -lpthread -DGOOS=\"freebsd\" -DGIT_REVISION=\"$(git rev-parse HEAD)\"
 cp ~/syzkaller/syz-executor ~/
